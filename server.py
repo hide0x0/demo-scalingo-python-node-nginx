14736343aa1e4cebacc50922b3dc610d9b7a90c6
uhide/u <106764598+hide0x0@users.noreply.github.com>
2024-12-01 18:22:53 +0600

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        self.wfile.write(b'Hello world, I am Python')
+        self.wfile.write(b'<h1>Subdomain Takeover By Abir</h1>')
 
 
 httpd = socketserver.TCPServer(('', 8001), Handler)
